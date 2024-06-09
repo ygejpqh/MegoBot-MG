0b6174b77732801e1a6c0144262acee610076beb
Botmego11 <166351897+Botmego11@users.noreply.github.com>
2024-06-09 12:08:25 +0300

    Update play-song.js

--- plugins/play-song.js
+++ plugins/play-song.js
@@ -29,7 +29,7 @@ const texto1 = `اغــنيــههة 乂 يـوتـيـوب\n
 await conn.sendButton(m.chat, texto1, wm, res.videos[0].thumbnail, [
 	['الصوت 📀', `${usedPrefix}mp3 ${text}`],
 	['الفيديو 🎥', `${usedPrefix}mp4 ${text}`]
-  ], null, [['قناتي ⚡', `${channel}`]], m)
+  ], null, [['قناتي ⚡', `https://whatsapp.com/channel/0029VaRygQcATRSk29RI4P1x`]], m)
 	  }
   
             if (command == "mp3") {
