eb29e139d9b2d5796dbe58ef758b0f080cb5594a
Botmego11 <166351897+Botmego11@users.noreply.github.com>
2024-06-22 14:08:04 +0300

    Update grop-poll.js

--- plugins/grop-poll.js
+++ plugins/grop-poll.js
@@ -1,3 +1,6 @@
+//ميجو يحبكم 🤭❤️
+//قناه المصدر
+//https://whatsapp.com/channel/0029VaRygQcATRSk29RI4P1x
 
 const handler = async (m, {conn, text, args, usedPrefix, command}) => {
   let name = await conn.getName(m.sender);
